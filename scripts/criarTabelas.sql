e46150b70580863e0a462244290653976a40e214
Lukas <lfta@cesar.school>
2025-04-10 07:28:06 -0300

    Adicionado os Scripts SQL

---
+++ scripts/criarTabelas.sql
@@ -0,0 +1,117 @@
+CREATE DATABASE bd_forja
+
+CREATE TABLE Loja (
+	ID_loja INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	nome VARCHAR(40),
+	cidade VARCHAR(40),
+	rua VARCHAR(40)
+);
+
+CREATE TABLE Ferreiro (
+	ID_ferreiro INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	nome VARCHAR(40),
+	especializacao VARCHAR(40)
+);
+
+CREATE TABLE Cliente (
+	ID_cliente INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	nome VARCHAR(40)
+);
+
+CREATE TABLE Item (
+	ID_item INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	nome VARCHAR(40),
+	valor FLOAT,
+	peso FLOAT,
+	raridade VARCHAR(40)
+);
+
+CREATE TABLE Arma (
+	Item INT AUTO_INCREMENT,
+	nome VARCHAR(40),
+	dano FLOAT,
+	tipo VARCHAR(40),
+	FOREIGN KEY(Item) REFERENCES Item(ID_item),
+	PRIMARY KEY(Item)
+);
+
+CREATE TABLE Armadura (
+	Item INT AUTO_INCREMENT,
+	nome VARCHAR(40),
+	defesa FLOAT,
+	tipo VARCHAR(40),
+	FOREIGN KEY(Item) REFERENCES Item(ID_item),
+	PRIMARY KEY(Item)
+);
+
+CREATE TABLE Material (
+	ID_material INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	nome VARCHAR(40),
+	quantidade INT,
+	qualidade VARCHAR(40),
+	tipo VARCHAR(40)
+);
+
+CREATE TABLE Fornecedor (
+	ID_material INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	nome VARCHAR(40),
+	tipo_material VARCHAR(40)
+);CREATE DATABASE bd_forja
+
+CREATE TABLE Loja (
+	ID_loja INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	nome VARCHAR(40),
+	cidade VARCHAR(40),
+	rua VARCHAR(40)
+);
+
+CREATE TABLE Ferreiro (
+	ID_ferreiro INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	nome VARCHAR(40),
+	especializacao VARCHAR(40)
+);
+
+CREATE TABLE Cliente (
+	ID_cliente INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	nome VARCHAR(40)
+);
+
+CREATE TABLE Item (
+	ID_item INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	nome VARCHAR(40),
+	valor FLOAT,
+	peso FLOAT,
+	raridade VARCHAR(40)
+);
+
+CREATE TABLE Arma (
+	Item INT AUTO_INCREMENT,
+	nome VARCHAR(40),
+	dano FLOAT,
+	tipo VARCHAR(40),
+	FOREIGN KEY(Item) REFERENCES Item(ID_item),
+	PRIMARY KEY(Item)
+);
+
+CREATE TABLE Armadura (
+	Item INT AUTO_INCREMENT,
+	nome VARCHAR(40),
+	defesa FLOAT,
+	tipo VARCHAR(40),
+	FOREIGN KEY(Item) REFERENCES Item(ID_item),
+	PRIMARY KEY(Item)
+);
+
+CREATE TABLE Material (
+	ID_material INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	nome VARCHAR(40),
+	quantidade INT,
+	qualidade VARCHAR(40),
+	tipo VARCHAR(40)
+);
+
+CREATE TABLE Fornecedor (
+	ID_material INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	nome VARCHAR(40),
+	tipo_material VARCHAR(40)
+);
